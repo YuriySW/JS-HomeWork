5b45f1bc1a7cf1cb145a7cf5e4f2b2d424ca9a8b
YuriySW <yuriy.s@my.com>
2023-12-20 14:41:39 +0500

    lesson 4.6 hw

--- lesson06/Task02.js
+++ lesson06/Task02.js
@@ -2,13 +2,6 @@
 
 const allCashbox = [4500, 3210, 650, 1250, 7830, 990, 13900, 370];
 
-const getAverageValue = (checks) => {
-  let result = 0;
-
-  for (var i = 0; i < checks.length; i++) {
-    result += checks[i];
-  }
-  return Math.floor(result / checks.length);
-};
+const getAverageValue = (checks) => Math.floor(checks.reduce((sum, current) => sum + current / checks.length, 0));
 
 console.log(getAverageValue(allCashbox));
