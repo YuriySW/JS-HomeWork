c28c380704379c71c9e2ed3e31e69955c8986161
YuriySW <yuriy.s@my.com>
2023-12-28 19:02:17 +0500

    lesson 4.7 hw 1.0

--- lesson04/Task02.js
+++
@@ -1,7 +0,0 @@
-const modificationString = (str) => {
-  const clearStr = str.trim().toLowerCase();
-
-  return clearStr[0].toUpperCase() + clearStr.substring(1);
-};
-
-console.log(modificationString('   ПривеТ Мир'));
