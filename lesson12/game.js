3aa9eaf5fb0ab0c40918a3dff54e86452126756d
YuriySW <yuriy.s@my.com>
2024-02-19 02:46:49 +0500

    lesson 4.12 HW 1.0

---
+++ lesson12/game.js
@@ -0,0 +1,86 @@
+'use strict';
+
+(() => {
+  const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
+
+  const getRandomIntInclusive = (min, max) => {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+  };
+
+  const game = () => {
+    const result = {
+      player: 0,
+      computer: 0,
+      draw: 0,
+    };
+
+    return function start() {
+      const randomIndex = getRandomIntInclusive(0, FIGURES_RUS.length - 1);
+      const randomWordComputer = FIGURES_RUS[randomIndex];
+      let userWord = prompt('Камень, ножницы, бумага?');
+      let whoWin;
+
+      const gameExit = () => {
+        const exit = confirm('Точно ли вы хотите выйти?');
+        if (exit) {
+          return alert(
+            `Результат:\n Компьютер: ${result.computer}\n Игрок: ${result.player}\n Ничья: ${result.draw}`
+          );
+        } else {
+          return start();
+        }
+      };
+
+      if (userWord !== null) {
+        switch (userWord.toLowerCase()) {
+          case 'к':
+          case 'кам':
+          case 'камень':
+            userWord = 'камень';
+            break;
+          case 'н':
+          case 'нож':
+          case 'ножницы':
+            userWord = 'ножницы';
+            break;
+          case 'б':
+          case 'бум':
+          case 'бумага':
+            userWord = 'бумага';
+            break;
+          default:
+            return start();
+        }
+      } else {
+        return gameExit();
+      }
+
+      if (userWord === randomWordComputer) {
+        whoWin = 'Ничья';
+        result.draw++;
+      } else if (
+        (userWord === 'камень' && randomWordComputer === 'ножницы') ||
+        (userWord === 'ножницы' && randomWordComputer === 'бумага') ||
+        (userWord === 'бумага' && randomWordComputer === 'камень')
+      ) {
+        whoWin = 'Вы выиграли';
+        result.player++;
+      } else {
+        whoWin = 'Компьютер выиграл!';
+        result.computer++;
+      }
+
+      alert(`Компьютер:${randomWordComputer}\nВы:${userWord}\n${whoWin}\n`);
+
+      const agreement = confirm('Ещё?');
+      if (agreement) {
+        return start();
+      } else {
+        return gameExit();
+      }
+    };
+  };
+  window.RPS = game;
+})();
