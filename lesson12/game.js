bdd8de41e0c147f1e26dae52afe22cdf8d9ee980
YuriySW <yuriy.s@my.com>
2024-02-22 16:23:38 +0500

    lesson 4.12 HW 1.6

--- lesson12/game.js
+++ lesson12/game.js
@@ -11,88 +11,48 @@
   };
 
   const game = (language) => {
+    const lang = language === 'EN' || language === 'ENG' ? FIGURES_ENG : FIGURES_RUS;
     const result = {
       player: 0,
       computer: 0,
     };
 
-    const lang = language === 'EN' || language === 'ENG' ? FIGURES_ENG : FIGURES_RUS;
-
     return function start() {
-      const randomWordComputer = getRandomIntInclusive(0, 2);
+      const randomComputer = getRandomIntInclusive(0, 2);
       let userWord;
-
-      if (lang === FIGURES_ENG) {
-        userWord = prompt('rock, scissors, paper?');
-      } else {
-        userWord = prompt('Камень, ножницы, бумага?');
-      }
-
       let whoWin;
+      userWord = prompt(`${lang}?`);
 
       const gameExit = () => {
-        const exit = confirm('Точно ли вы хотите выйти?');
-
-        if (exit) {
-          return alert(`Результат:\n Компьютер: ${result.computer}\n Игрок: ${result.player}\n`);
-        } else {
-          return start();
-        }
+        confirm('Точно ли вы хотите выйти?')
+          ? alert(`Результат:\n Компьютер: ${result.computer}\n Игрок: ${result.player}\n`)
+          : start();
       };
 
       const alertWindow = () =>
-        lang === FIGURES_ENG
-          ? alert(`Компьютер:${FIGURES_ENG[randomWordComputer]}\nВы:${userWord}\n${whoWin}\n`)
-          : alert(`Компьютер:${FIGURES_RUS[randomWordComputer]}\nВы:${userWord}\n${whoWin}\n`);
+        alert(`Компьютер:${lang[randomComputer]}\nВы:${lang[userWord]}\n${whoWin}\n`);
 
       if (userWord !== null) {
-        if (lang === FIGURES_ENG) {
-          switch (userWord.toLowerCase()) {
-            case 'rock':
-              userWord = 'rock';
-              break;
-            case 'scissors':
-              userWord = 'scissors';
-              break;
-            case 'paper':
-              userWord = 'paper';
-              break;
-            default:
-              return start();
-          }
+        const userIndex = lang.indexOf(userWord.toLowerCase());
+        if (userIndex !== -1) {
+          userWord = userIndex;
         } else {
-          switch (userWord.toLowerCase()) {
-            case 'камень':
-              userWord = 'камень';
-              break;
-            case 'ножницы':
-              userWord = 'ножницы';
-              break;
-            case 'бумага':
-              userWord = 'бумага';
-              break;
-            default:
-              return start();
-          }
+          return start();
         }
       } else {
         return gameExit();
       }
 
-      if (
-        userWord === FIGURES_RUS[randomWordComputer] ||
-        userWord === FIGURES_ENG[randomWordComputer]
-      ) {
+      if (userWord === randomComputer) {
         whoWin = 'Ничья';
         alertWindow();
         return start();
-      } else if (
-        (userWord === FIGURES_RUS[0] && FIGURES_RUS[randomWordComputer] === FIGURES_RUS[1]) ||
-        (userWord === FIGURES_RUS[1] && FIGURES_RUS[randomWordComputer] === FIGURES_RUS[2]) ||
-        (userWord === FIGURES_RUS[2] && FIGURES_RUS[randomWordComputer] === FIGURES_RUS[0]) ||
-        (userWord === FIGURES_ENG[0] && FIGURES_ENG[randomWordComputer] === FIGURES_ENG[1]) ||
-        (userWord === FIGURES_ENG[1] && FIGURES_ENG[randomWordComputer] === FIGURES_ENG[2]) ||
-        (userWord === FIGURES_ENG[2] && FIGURES_ENG[randomWordComputer] === FIGURES_ENG[0])
+      }
+
+      if (
+        (userWord === 0 && randomComputer === 1) ||
+        (userWord === 1 && randomComputer === 2) ||
+        (userWord === 2 && randomComputer === 0)
       ) {
         whoWin = 'Вы выиграли';
         result.player++;
